4e721c00519b93a310e0a1d34e661c2ab09dd023
satoung <satoung@gmail.com>
2017-05-23 15:29:30 +0700

    Add MusicController.java

---
+++ src/main/java/tam/musicplayer/MusicController.java
@@ -0,0 +1,17 @@
+package tam.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Equinnox Zhetena on 5/23/2017.
+ */
+
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+}
